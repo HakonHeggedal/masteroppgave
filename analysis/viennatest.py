b421dc263c226f225109ae1dc2b98b5563a7d59a
Hakon <hakonheggedal@gmail.com>
2014-09-23 12:55:48 +0200

    gitignore
    testing call/return vienna with a sequence

---
+++ analysis/viennatest.py
@@ -0,0 +1,30 @@
+
+
+
+# import subprocess
+from subprocess import PIPE, Popen
+# print subprocess.call("dir")
+
+# subprocess.check_call("rnafold")
+
+
+seq = "uuggauguuggccuaguucuguguggaagacuagugauuuuguuguuuuuagauaacuaaa" \
+      "ucgacaacaaaucacagucugccauauggcacaggccaugccucuacag"
+
+seq2 = "uuggauguuggccuaguucuguguggaagacuagugauuuuguuguuuuuagauaacuaaaucgacaacaaaucacagucugccauauggcacaggccaugccucuacag"
+
+
+
+command = "rnafold"
+
+mypipe  = Popen(command, stdin=PIPE, stdout=PIPE, bufsize=-1)
+
+ans, errs = mypipe.communicate(seq2) 
+
+fold, eng = ans.split(" ")
+energy = float(eng.strip().strip("()"))
+
+print fold
+print energy
+
+print errs
