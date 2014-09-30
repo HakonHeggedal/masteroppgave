a69a6db5d5e68cf6afc357042d3965ba144c479d
hakon <hakon@hakon-skole>
2014-09-30 18:15:56 +0200

    read sequence file

--- analysis/reads.py
+++ analysis/reads.py
@@ -2,4 +2,18 @@
 # all RNA lines in the files
 rnas = [l for l in open("mature.fa") if l[0] is not ">"]
 
-
+def readcollapsed(filename):
+    
+    freqdick = {}
+    
+    with open(filename, "r") as inputfile:
+        
+        for line in inputfile:
+            line = line.strip()
+            count = 0
+            if line[0] == ">":
+                count = int(line.split(-)[1])
+            elif count > 0:
+                freqdick[line] = count
+            
+            
