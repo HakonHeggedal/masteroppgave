04d630b2e125aa3f7fdfdb86d1856d8ffba11d29
hakon <hakon@hakon-skole>
2014-10-31 15:42:54 +0100

    merging collapsed files should work now

--- inputs/merge.py
+++ inputs/merge.py
@@ -14,16 +14,17 @@ def merge_collapsed_fasta(fasta_files):
         
         with open(filename) as fasta_file:
             
+            count = 0
             for line in fasta_file:
                 line = line.strip()
                 if line[0] == ">":
                     count = int(line.split("-")[1])
-                elif len(line) > 10:
-                    
+                elif len(line) > 10 and count > 0 and _legal_DNA(line):
                     if line in all_sequences:
                         all_sequences[line] += count
                     else:
                         all_sequences[line] = count
+                    count = 0
     
     small_reads = []
     small_reads_count = []
@@ -38,7 +39,24 @@ def merge_collapsed_fasta(fasta_files):
         else:
             reads.append(seq)
             reads_count.append(count)
-            
+    
+    print len(reads), len(small_reads), len(reads_count), len(small_reads_count)
     
     return reads, reads_count, small_reads, small_reads_count
+
+
+
+
+def _legal_DNA(sequence, chars=set("ACGT")):
     
+    for char in sequence:
+        if char not in chars:
+#             print char
+            return False
+    
+    return True
+
+
+fastas = ["SRR797060.collapsed", "SRR797061.collapsed", "SRR797062.collapsed", "SRR797063.collapsed"]
+
+merge_collapsed_fasta(fastas)
